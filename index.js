80d7a5f76f759310a4737dfe98c19235b222d7bc
varshil shah <varshil.shah@ecosmob.com>
2023-09-22 13:44:02 +0530

    Initial COmmit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const data = require('./dummyData')
 const app = express();
-app.listen(3000, () => {
+const port = process.env.port || 4500
+app.listen(port, () => {
   console.log("App is started on 3000");
 });
 app.get('/', (req, res) => {
